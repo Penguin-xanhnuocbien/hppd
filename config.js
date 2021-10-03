1c2243f22a3795b3602c1dbcf2e76136466fd3fd
Penguin-xanhlam <85724679+Penguin-xanhlam@users.noreply.github.com>
2021-10-03 16:19:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'hello mng ^^',
+    introDesc: `mng bt hôm nay là ngày gì không nhỉ =)))`,
+    btnIntro: 'biết chứ',
+    title: 'Hôm nay 1 thiên thần đáng iu nhất vũ trụ đã ra đời đóoo',
+    desc: ' mng cũng đoán đc đó là ai đúng kooo =))))) ',
+    btnYes: 'Thạch Nguyễn đz cute chứ ai nữa',
+    btnNo: 'Ờ không ?? :3',
+    question:'Gửi lời chúc tốt đẹp đến tuii đi ^^',
+    btnReply: 'Gửi cho cậu <3',
+    reply: 'Đùa thế chứ zô fb hay mess mà chúc ai lại chúc ở đây <33333333',
+    mess: 'Yêu mng nhiều nhieefuu <3333333',
+    messDesc: 'Nhớ zô chúc đó nhaaa',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/thach3619/' 
 }
